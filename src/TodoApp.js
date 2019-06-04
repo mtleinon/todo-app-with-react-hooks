ecab94a766bc1a67b5b1372ba993465c62e90073
mtleinon <mika.t.leinonen@gmail.com>
2019-06-05 00:06:32 +0300

    Added storing off todos to local storage after each update.

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,12 +8,17 @@ import uuid from 'uuid';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 function TodoApp() {
-  const initialTodos = [
-    { id: 1, task: 'Clean fistank', completed: false },
-    { id: 2, task: 'Wash car', completed: true },
-    { id: 3, task: 'Grow beard', completed: false }
-  ];
+  const initialTodos = JSON.parse(window.localStorage.getItem('todos')) || [];
+  // const initialTodos = [
+  //   { id: 1, task: 'Clean fistank', completed: false },
+  //   { id: 2, task: 'Wash car', completed: true },
+  //   { id: 3, task: 'Grow beard', completed: false }
+  // ];
   const [todos, setTodos] = useState(initialTodos);
+
+  useEffect(() => {
+    window.localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
   const addTodo = newTodoText => {
     setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
   };
